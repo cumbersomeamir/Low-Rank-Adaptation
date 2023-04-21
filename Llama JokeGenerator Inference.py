f8aa5416a62202a82143fe8499baec7eeedc3870
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-04-21 14:17:15 +0100

    Update Llama JokeGenerator Inference.py

--- Llama JokeGenerator Inference.py
+++ Llama JokeGenerator Inference.py
@@ -2,24 +2,26 @@
 '''
 
 
+import os
 
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+import bitsandbytes as bnb
+import torch
+from peft import PeftModel, PeftConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-# Load the tokenizer and model
-model_name = "Amirkid/LlamaJokeGenerator"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+peft_model_id = "Amirkid/LlamaJokeGenerator"
+config = PeftConfig.from_pretrained(peft_model_id)
+model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, return_dict=True, load_in_8bit=True, device_map='auto')
+tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
 
-# Create a pipeline for text generation
-generator = pipeline("text-generation", model=model, tokenizer=tokenizer)
+# Load the Lora model
+model = PeftModel.from_pretrained(model, peft_model_id)
 
-# Input text
-text = "What a beautiful day in london"
+batch = tokenizer("Amir opened up with this joke", return_tensors='pt')
 
-# Generate a completion
-result = generator(text, max_length=50, do_sample=True, temperature=0.7)
+with torch.cuda.amp.autocast():
+  output_tokens = model.generate(**batch, max_new_tokens=50)
 
-# Print the result
-print(result[0]['generated_text'])
-
+print('\n\n', tokenizer.decode(output_tokens[0], skip_special_tokens=True))
 
