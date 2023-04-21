a1920355231111aa47dae13a7cad3b000823cd74
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-04-21 14:24:28 +0100

    Update Llama JokeGenerator Inference.py

--- Llama JokeGenerator Inference.py
+++ Llama JokeGenerator Inference.py
@@ -1,27 +1,29 @@
-'''!pip3 install bitsandbytes datasets accelerate loralib sentencepiece datasets transformers git+https://github.com/huggingface/transformers.git@main git+https://github.com/huggingface/peft.git
-'''
-
-
+"""!pip install --upgrade bitsandbytes datasets accelerate loralib sentencepiece transformers git+https://github.com/huggingface/transformers.git@main git+https://github.com/huggingface/peft.git
+"""
 import os
-
 import bitsandbytes as bnb
 import torch
 from peft import PeftModel, PeftConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 peft_model_id = "Amirkid/LlamaJokeGenerator"
 config = PeftConfig.from_pretrained(peft_model_id)
-model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, return_dict=True, load_in_8bit=True, device_map='auto')
+model = AutoModelForCausalLM.from_pretrained(
+    config.base_model_name_or_path, return_dict=True, load_in_8bit=True, device_map="auto"
+)
 tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
 
 # Load the Lora model
 model = PeftModel.from_pretrained(model, peft_model_id)
 
-batch = tokenizer("Amir opened up with this joke", return_tensors='pt')
+batch = tokenizer("Amir opened up with this joke", return_tensors="pt")
 
 with torch.cuda.amp.autocast():
-  output_tokens = model.generate(**batch, max_new_tokens=50)
+    output_tokens = model.generate(
+        input_ids=batch["input_ids"],
+        attention_mask=batch["attention_mask"],
+        max_new_tokens=50,
+    )
 
-print('\n\n', tokenizer.decode(output_tokens[0], skip_special_tokens=True))
-
+print("\n\n", tokenizer.decode(output_tokens[0], skip_special_tokens=True))
